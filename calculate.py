be26de335b54399837ebd6632984ca26376d12bf
bvvreg <bvvreg@gmail.com>
2022-07-12 21:46:11 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -3,26 +3,33 @@ import sys
 print ("\n********** CALCULATOR ***********")
 print ("*     Арифметичні операції      *")
 print ("*         (+, -, *, /)          *")
-print ("*     з двома аргументами       *")
+print ("*  з двома цілими аргументами   *")
 print ("*     заданими у вигляді:       *")
 print ("*         X операція Y          *")
 print ("*     в командній стрічці       *")
 print ("*********************************\n")
-Arg = sys.argv
-KolArg = len(Arg)
-if KolArg == 4:
-    match Arg[2]:
-        case "+":
-            print ("Сума чисел:", int(Arg[1]) + int(Arg[3]))
-        case "-":
-            print ("Різниця чисел:", int(Arg[1]) - int(Arg[3]))
-        case "*":
-            print ("Добуток чисел:", int(Arg[1]) * int(Arg[3]))
-        case "/":
-            if int(Arg[3]) == 0:
-                print ("Помилка: ділення на 0!")
-            else:
-                print ("Частка чисел:", int(Arg[1]) / int(Arg[3]))
-else:
-    print ("Недопустима кількість аргументів!")
+SpysokArgumentiv = sys.argv
+Operaciya = SpysokArgumentiv[2]
+
+try:
+    Argument1 = int(SpysokArgumentiv[1])
+    Argument2 = int(SpysokArgumentiv[3])
+    KilArg = len(SpysokArgumentiv)
+    if KilArg == 4:
+        match Operaciya:
+            case "+":
+                print ("Сума чисел:", int(Argument1) + int(Argument2))
+            case "-":
+                print ("Різниця чисел:", int(Argument1) - int(Argument2))
+            case "*":
+                print ("Добуток чисел:", int(Argument1) * int(Argument2))
+            case "/":
+                if int(Argument2) == 0:
+                    print ("Помилка: ділення на 0!")
+                else:
+                    print ("Частка чисел:", int(Argument1) / int(Argument2))
+    else:
+        print ("Помилка: недопустима кількість аргументів!")
+except ValueError:
+    print ("Помилка:введіть числа!")
 print ("\n*********************************")
