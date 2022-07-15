3736456c12ad243966945b5d2916884b96aee1ff
bvvreg <bvvreg@gmail.com>
2022-07-15 21:20:15 +0300

    L2

---
+++ exercise-additional-2.py
@@ -0,0 +1,24 @@
+# Напишіть функцію commonStr що задовільняє наступній умові:
+
+
+
+# Дано дві строчки, поверніть нову строчку 
+# яка буде складатись виключно зі спільних символів перерадних строк.
+# Якщо немає спільних символів - поверніть пусту строчку. 
+# Ви можете повернути відповідь у будь-якому порядку.
+# Символи не повині дублюватись у відповіді.
+
+# Приклад 1
+# commonStr('loli', 'luck') -> 'l'
+
+# Приклад 1
+# commonStr('good day', 'good morning') -> 'god'
+
+
+def commonStr(str1, str2):
+    return str(''.join(set(str1) & set(str2)))
+
+print(commonStr('loli', 'luck'))
+print(commonStr('loli', 'luck') == 'l') 
+print(commonStr('good day', 'good morning')) 
+print(commonStr('good day', 'good morning') == 'god') 
